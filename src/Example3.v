bf20046c62897352fc7eb205d9fc2d5d04cd9499
Bartłomiej Królikowski <bartlomiej.krolikowski@yahoo.com>
2024-02-17 00:12:18 +0100

    defined a tactic solving simple computation proofs

--- src/Example3.v
+++ src/Example3.v
@@ -69,80 +69,58 @@ Proof.
   exists 0. constructor.
 Qed.
 
+Ltac solve_cost_red x :=
+  repeat (eapply no_red || (eapply S_red; [ x | ])).
+
+Ltac solve_red :=
+  repeat (cbn; (
+    match goal with
+    | [ |- exists _, _ ] => eexists
+    | [ |- Is_fresh_label _ _ ] =>
+      unfold Is_fresh_label; try easy; shelve
+    | [ |- _ /\ _ ] => split
+    | [ |- Lookup ?l ((?l, _) :: _ )%list _ ] => eapply Lookup_hd
+    | [ |- Lookup _ _ _ ] => eapply Lookup_tl
+    | [ |- Assignment ?l _ ((?l, _) :: _ )%list _ ] => eapply Assignment_hd
+    | [ |- Assignment _ _ _ _ ] => eapply Assignment_tl
+    | [ |- cost_red ?e _ ?e _ _ ] => eapply no_red
+    | [ |- cost_red _ _ _ _ _ ] => eapply S_red
+    | [ |- red (App (Val _) (Val _)) _ _ _ ] => eapply red_lam
+    | [ |- red (Ref (Val _)) _ _ _ ] => eapply red_ref
+    | [ |- red (Deref (Val _)) _ _ _ ] => eapply red_deref
+    | [ |- red (Assign (Val _) (Val _)) _ _ _ ] => eapply red_assign
+    | [ |- red (Seq (Val _) (Val _)) _ _ _ ] => eapply red_seq
+    | [ |- red (App (Val _) _) _ _ _ ] => eapply red_app2
+    | [ |- red (App _ _) _ _ _ ] => eapply red_app1
+    | [ |- red (Ref _) _ _ _ ] => eapply red_ref_e
+    | [ |- red (Deref _) _ _ _ ] => eapply red_deref_e
+    | [ |- red (Assign (Val _) _) _ _ _ ] => eapply red_assign2
+    | [ |- red (Assign _ _) _ _ _ ] => eapply red_assign1
+    | [ |- red (Seq (Val _) _) _ _ _ ] => eapply red_seq2
+    | [ |- red (Seq _ _) _ _ _ ] => eapply red_seq1
+    end
+  )).
+
+Ltac solve_computation := solve_cost_red solve_red.
+
 (* interesting proof *)
 Goal exists l m c,
   cost_red e []%list (Lab l) m c /\
     List.In (l, U_val) m.
 Proof.
   unfold e.
-  do 30 try (cbn; (
-    match goal with
-    | [ |- exists _, _ ] => eexists
-    end
-    + match goal with
-      | [ |- _ /\ _ ] => split
-      end
-    + easy
-    + match goal with
-      | [ |- Lookup ?l ((?l, _) :: _ )%list _ ] => eapply Lookup_hd
-      | [ |- Lookup _ _ _ ] => eapply Lookup_tl
-      end
-    + match goal with
-      | [ |- Assignment ?l _ ((?l, _) :: _ )%list _ ] => eapply Assignment_hd
-      | [ |- Assignment _ _ _ _ ] => eapply Assignment_tl
-      end
-    + match goal with
-      | [ |- cost_red ?e _ ?e _ _ ] => eapply no_red
-      | [ |- cost_red _ _ _ _ _ ] => eapply S_red
-      end
-    + match goal with
-      | [ |- red (App (Val _) (Val _)) _ _ _ ] => eapply red_lam
-      end
-    + match goal with
-      | [ |- red (Ref (Val _)) _ _ _ ] => eapply red_ref
-      end
-    + match goal with
-      | [ |- red (Deref (Val _)) _ _ _ ] => eapply red_deref
-      end
-    + match goal with
-      | [ |- red (Assign (Val _) (Val _)) _ _ _ ] => eapply red_assign
-      end
-    + match goal with
-      | [ |- red (Seq (Val _) (Val _)) _ _ _ ] => eapply red_seq
-      end
-    + match goal with
-      | [ |- red (App (Val _) _) _ _ _ ] => eapply red_app2
-      end
-    + match goal with
-      | [ |- red (App _ _) _ _ _ ] => eapply red_app1
-      end
-    + match goal with
-      | [ |- red (Ref _) _ _ _ ] => eapply red_ref_e
-      end
-    + match goal with
-      | [ |- red (Deref _) _ _ _ ] => eapply red_deref_e
-      end
-    + match goal with
-      | [ |- red (Assign (Val _) _) _ _ _ ] => eapply red_assign2
-      end
-    + match goal with
-      | [ |- red (Assign _ _) _ _ _ ] => eapply red_assign1
-      end
-    + match goal with
-      | [ |- red (Seq (Val _) _) _ _ _ ] => eapply red_seq2
-      end
-    + match goal with
-      | [ |- red (Seq _ _) _ _ _ ] => eapply red_seq1
-      end
-    + match goal with
-      | [ |- red (subst_e _ _) _ _ _ ] => cbn
-      end
-    + match goal with
-      | [ |- red (Val _) _ _ _ ] => fail
-      end
-  ); cbn).
-  2:econstructor.
-  2:econstructor.
-  2:econstructor.
-unfold Is_fresh_label. simpl.
+  repeat eexists.
+  {
+    solve_computation.
+  }
+  {
+    simpl. eauto.
+  }
+  Unshelve.
+  { econstructor. exact 0. }
+  { exact []%list. }
+  { econstructor. exact 1. }
+  { exact []%list. }
+  { exact []%list. }
+  { simpl. intros [ ? | [] ]. easy. }
 Qed.
