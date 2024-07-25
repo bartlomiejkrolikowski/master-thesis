89fa43783c446b038f742d361cac4a1088210a94
Bartłomiej Królikowski <bartlomiej.krolikowski@yahoo.com>
2024-07-25 11:18:28 +0200

    renamed Dijkstra_simple to Dijkstra_generic

--- src_arrays/Dijkstra_generic.v
+++ src_arrays/Dijkstra_generic.v
@@ -18,10 +18,21 @@ Definition init_array : Value string :=
       [while] ! Var "i" [<] Var "size" [do]
         (Var "array" >> ! Var "i") <- Var "value";;
         Var "i" <- ! Var "i" [+] Int 1
-      [end]
+      [end];;
+      Free (Var "i")
     [end]%string.
 
-Definition generic_dijkstra (get_size get_neighbours mkheap h_insert h_empty h_extract_min h_decrease_key l_is_nil l_head l_tail : Value string) : Value string :=
+Definition free_array : Value string :=
+  [-\] "array", [-\] "size",
+    [let "i"] Ref (Int 0) [in]
+      [while] ! Var "i" [<] Var "size" [do]
+        Free (Var "array" >> ! Var "i");;
+        Var "i" <- ! Var "i" [+] Int 1
+      [end];;
+      Free (Var "i")
+    [end]%string.
+
+Definition generic_dijkstra (get_size get_neighbours mkheap h_insert h_empty h_extract_min h_decrease_key h_free l_is_nil l_head l_tail : Value string) : Value string :=
   [-\] "g", [-\] "src", [-\] "dst",
     [let "n"] get_size <* Var "g" [in]
     [let "h"] mkheap <* Var "n" [in]
@@ -56,8 +67,32 @@ Definition generic_dijkstra (get_size get_neighbours mkheap h_insert h_empty h_e
         [end]
         [end]
         [end]
+      [end];;
+      h_free <* (Var "h");;
+      [let "x"] ! (Var "dist" >> Var "dst") [in]
+        free_array <* (Var "dist");;
+        Var "x"
       [end]
     [end]
     [end]
     [end]
     [end]%string.
+
+Parameter is_graph : forall {V}, Label -> StateAssertion V.
+Parameter dists_in_graph : forall {V}, Label -> Label -> StateAssertion V.
+Parameter in_graph : forall {V}, Label -> Label -> StateAssertion V.
+Parameter dist_in_graph : forall {V}, Label -> Z -> Z -> nat -> StateAssertion V.
+
+Parameter get_size get_neighbours mkheap h_insert h_empty h_extract_min h_decrease_key h_free l_is_nil l_head l_tail : Value string.
+
+Theorem triple_fun_generic_dijkstra l src dst :
+  triple_fun
+    (generic_dijkstra get_size get_neighbours mkheap h_insert h_empty h_extract_min h_decrease_key h_free l_is_nil l_head l_tail)
+    (fun v => <[v = Lab l]>)
+    (fun v => <[triple_fun v
+      (fun v => <[v = Int src]>)
+      (fun v => <[triple_fun v
+        (fun v => <[v = Int dst]> <*> is_graph l)
+        (fun v => <exists> n, <[v = Int (Z.of_nat n)]> <*> is_graph l <*> dist_in_graph l src dst n)]>)]>).
+Proof.
+Admitted.
